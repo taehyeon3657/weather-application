46cf563b42160e2e21ebbcb7f738f2c8adfc4f78
taehyeon3657 <taehyeon3657@gmail.com>
2026-02-05 18:46:53 +0900

    feat: vercel analytics 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import QueryProvider from '@/shared/lib/tanstackQuery/query.provider';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -43,6 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Analytics />
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
